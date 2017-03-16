6e41fe06eea482850a5e7564803e1a05624f3da9
rossf7 <ross@giantswarm.io>
2017-03-16 10:20:37 +0100

    Revert "K8s client gets config from viper and add validation checks for the kubernetes api address."
    This reverts commit 211413a4154922960f2313d9891ec5e8c3f4c69f.

--- client/k8s/k8s_test.go
+++ client/k8s/k8s_test.go
@@ -1,92 +1,133 @@
 package k8s
 
 import (
-	"os"
-
 	"fmt"
 	"testing"
 
-	"github.com/giantswarm/microkit/logger"
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
-
-	"github.com/giantswarm/cert-operator/flag"
+	"k8s.io/client-go/rest"
 )
 
 func TestGetRawClientConfig(t *testing.T) {
-	var err error
-	var newLogger logger.Logger
-
-	{
-		loggerConfig := logger.DefaultConfig()
-		loggerConfig.IOWriter = os.Stdout
-		newLogger, err = logger.New(loggerConfig)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	certFile := "/var/run/kubernetes/client-admin.crt"
-	keyFile := "/var/run/kubernetes/client-admin.key"
-	caFile := "/var/run/kubernetes/server-ca.crt"
-
 	tests := []struct {
 		name            string
-		inCluster       bool
+		config          Config
 		expectedError   bool
-		expectedAddress string
+		expectedHost    string
+		expectedCrtFile string
+		expectedKeyFile string
+		expectedCAFile  string
 	}{
 		{
-			name:            "Specify out-cluster config. It should return it. Use cert auth files.",
-			inCluster:       false,
-			expectedError:   false,
-			expectedAddress: "http://out-cluster-host",
+			name: "Specify only in-cluster config. It should return it. Use cert auth files.",
+			config: Config{
+				InCluster: true,
+				inClusterConfigProvider: func() (*rest.Config, error) {
+					return &rest.Config{
+						Host: "http://in-cluster-host",
+						TLSClientConfig: rest.TLSClientConfig{
+							CertFile: "/var/run/kubernetes/client-admin.crt",
+							KeyFile:  "/var/run/kubernetes/client-admin.key",
+							CAFile:   "/var/run/kubernetes/server-ca.crt",
+						},
+					}, nil
+				},
+			},
+			expectedHost:    "http://in-cluster-host",
+			expectedCrtFile: "/var/run/kubernetes/client-admin.crt",
+			expectedKeyFile: "/var/run/kubernetes/client-admin.key",
+			expectedCAFile:  "/var/run/kubernetes/server-ca.crt",
 		},
 		{
-			name:            "Specify out-cluster config. It should error due to missing k8s address.",
-			inCluster:       false,
-			expectedError:   true,
-			expectedAddress: "",
+			name: "Do not specify anything while using in-cluster config. It should return an error.",
+			config: Config{
+				InCluster: true,
+				inClusterConfigProvider: func() (*rest.Config, error) {
+					return nil, fmt.Errorf("No in-cluster config")
+				},
+			},
+			expectedError: true,
 		},
 		{
-			name:            "Specify out-cluster config. It should error due to invalid k8s address.",
-			inCluster:       false,
-			expectedError:   false,
-			expectedAddress: "invalid-host",
+			name: "Specify both in-cluster config and CLI config. It should return the in-cluster config. Use cert auth files.",
+			config: Config{
+				InCluster: true,
+				Address:   "http://host-from-cli",
+				TLSClientConfig: TLSClientConfig{
+					CertFile: "/var/run/kubernetes/client-cli.crt",
+					KeyFile:  "/var/run/kubernetes/client-cli.key",
+					CAFile:   "/var/run/kubernetes/server-ca-cli.crt",
+				},
+				inClusterConfigProvider: func() (*rest.Config, error) {
+					return &rest.Config{
+						Host: "http://in-cluster-host",
+						TLSClientConfig: rest.TLSClientConfig{
+							CertFile: "/var/run/kubernetes/client-admin.crt",
+							KeyFile:  "/var/run/kubernetes/client-admin.key",
+							CAFile:   "/var/run/kubernetes/server-ca.crt",
+						},
+					}, nil
+				},
+			},
+			expectedHost:    "http://in-cluster-host",
+			expectedCrtFile: "/var/run/kubernetes/client-admin.crt",
+			expectedKeyFile: "/var/run/kubernetes/client-admin.key",
+			expectedCAFile:  "/var/run/kubernetes/server-ca.crt",
 		},
 		{
-			name:            "Specify in-cluster config. Currently errors due to missing k8s env vars.",
-			inCluster:       true,
-			expectedError:   true, // TODO Get in-cluster config working in tests.
-			expectedAddress: "invalid-host",
+			name: "Specify both in-cluster config and CLI config. It should return the CLI config. Use cert auth files.",
+			config: Config{
+				Address: "http://host-from-cli",
+				TLSClientConfig: TLSClientConfig{
+					CertFile: "/var/run/kubernetes/client-cli.crt",
+					KeyFile:  "/var/run/kubernetes/client-cli.key",
+					CAFile:   "/var/run/kubernetes/server-ca-cli.crt",
+				},
+				inClusterConfigProvider: func() (*rest.Config, error) {
+					return &rest.Config{
+						Host: "http://in-cluster-host",
+						TLSClientConfig: rest.TLSClientConfig{
+							CertFile: "/var/run/kubernetes/client-admin.crt",
+							KeyFile:  "/var/run/kubernetes/client-admin.key",
+							CAFile:   "/var/run/kubernetes/server-ca.crt",
+						},
+					}, nil
+				},
+			},
+			expectedHost:    "http://host-from-cli",
+			expectedCrtFile: "/var/run/kubernetes/client-cli.crt",
+			expectedKeyFile: "/var/run/kubernetes/client-cli.key",
+			expectedCAFile:  "/var/run/kubernetes/server-ca-cli.crt",
+		},
+		{
+			name: "Specify only CLI config. It should return it. Use cert auth files.",
+			config: Config{
+				Address: "http://host-from-cli",
+				TLSClientConfig: TLSClientConfig{
+					CertFile: "/var/run/kubernetes/client-cli.crt",
+					KeyFile:  "/var/run/kubernetes/client-cli.key",
+					CAFile:   "/var/run/kubernetes/server-ca-cli.key",
+				},
+				inClusterConfigProvider: func() (*rest.Config, error) {
+					return nil, fmt.Errorf("No in-cluster config")
+				},
+			},
+			expectedHost:    "http://host-from-cli",
+			expectedCrtFile: "/var/run/kubernetes/client-cli.crt",
+			expectedKeyFile: "/var/run/kubernetes/client-cli.key",
+			expectedCAFile:  "/var/run/kubernetes/server-ca-cli.key",
 		},
 	}
 	for _, tc := range tests {
-		f := flag.New()
-		v := viper.New()
-
-		v.Set(f.Kubernetes.InCluster, tc.inCluster)
-		v.Set(f.Kubernetes.Address, tc.expectedAddress)
-		v.Set(f.Kubernetes.TLS.CertFile, certFile)
-		v.Set(f.Kubernetes.TLS.KeyFile, keyFile)
-		v.Set(f.Kubernetes.TLS.CAFile, caFile)
-
-		config := Config{
-			Logger: newLogger,
-
-			Flag:  f,
-			Viper: v,
-		}
-
-		rawClientConfig, err := getRawClientConfig(config)
+		rawClientConfig, err := getRawClientConfig(tc.config)
 		if tc.expectedError {
 			assert.Error(t, err, fmt.Sprintf("[%s] An error was expected", tc.name))
 			continue
 		}
 		assert.Nil(t, err, fmt.Sprintf("[%s] An error was unexpected", tc.name))
-		assert.Equal(t, tc.expectedAddress, rawClientConfig.Host, fmt.Sprintf("[%s] Hosts should be equal", tc.name))
-		assert.Equal(t, certFile, rawClientConfig.TLSClientConfig.CertFile, fmt.Sprintf("[%s] CertFiles should be equal", tc.name))
-		assert.Equal(t, keyFile, rawClientConfig.TLSClientConfig.KeyFile, fmt.Sprintf("[%s] KeyFiles should be equal", tc.name))
-		assert.Equal(t, caFile, rawClientConfig.TLSClientConfig.CAFile, fmt.Sprintf("[%s] CAFiles should be equal", tc.name))
+		assert.Equal(t, tc.expectedHost, rawClientConfig.Host, fmt.Sprintf("[%s] Hosts should be equal", tc.name))
+		assert.Equal(t, tc.expectedCrtFile, rawClientConfig.TLSClientConfig.CertFile, fmt.Sprintf("[%s] CertFiles should be equal", tc.name))
+		assert.Equal(t, tc.expectedKeyFile, rawClientConfig.TLSClientConfig.KeyFile, fmt.Sprintf("[%s] KeyFiles should be equal", tc.name))
+		assert.Equal(t, tc.expectedCAFile, rawClientConfig.TLSClientConfig.CAFile, fmt.Sprintf("[%s] CAFiles should be equal", tc.name))
 	}
 }
