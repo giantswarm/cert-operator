93b838ea58d0b5f4fff858cd247d353d3e0a6bb2
rossf7 <ross@giantswarm.io>
2017-03-16 13:39:12 +0100

    Add Vault API client

---
+++ client/vault/vault.go
@@ -0,0 +1,49 @@
+package vault
+
+import (
+	"net/http"
+	"net/url"
+
+	microerror "github.com/giantswarm/microkit/error"
+	vaultapi "github.com/hashicorp/vault/api"
+	"github.com/spf13/viper"
+
+	"github.com/giantswarm/cert-operator/flag"
+)
+
+type Config struct {
+	// Settings.
+	Flag  *flag.Flag
+	Viper *viper.Viper
+}
+
+func NewClient(config Config) (*vaultapi.Client, error) {
+	address := config.Viper.GetString(config.Flag.Vault.Address)
+	token := config.Viper.GetString(config.Flag.Vault.Token)
+
+	if address == "" {
+		return nil, microerror.MaskAnyf(invalidConfigError, "vault address must not be empty")
+	}
+
+	// Check Vault address is valid.
+	_, err := url.ParseRequestURI(address)
+	if err != nil {
+		return nil, microerror.MaskAny(err)
+	}
+
+	if token == "" {
+		return nil, microerror.MaskAnyf(invalidConfigError, "vault address must not be empty")
+	}
+
+	newClientConfig := vaultapi.DefaultConfig()
+	newClientConfig.Address = address
+	newClientConfig.HttpClient = http.DefaultClient
+
+	newVaultClient, err := vaultapi.NewClient(newClientConfig)
+	if err != nil {
+		return nil, err
+	}
+	newVaultClient.SetToken(token)
+
+	return newVaultClient, nil
+}
---
+++ client/vault/vault_test.go
@@ -0,0 +1,67 @@
+package vault
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/giantswarm/cert-operator/flag"
+)
+
+func TestNewClient(t *testing.T) {
+	tests := []struct {
+		name          string
+		address       string
+		token         string
+		expectedError bool
+	}{
+		{
+			name:          "Specify vault address and token. It should return a vault client.",
+			address:       "http://localhost:8200",
+			token:         "auth-token",
+			expectedError: false,
+		},
+		{
+			name:          "Specify vault address but no token. It should return an error.",
+			address:       "http://localhost:8200",
+			token:         "",
+			expectedError: true,
+		},
+		{
+			name:          "Specify a vault token but no address. It should return an error.",
+			address:       "",
+			token:         "auth-token",
+			expectedError: true,
+		},
+		{
+			name:          "Specify an invalid vault address. It should return an error.",
+			address:       "http//invalid-address",
+			token:         "auth-token",
+			expectedError: true,
+		},
+	}
+
+	for _, tc := range tests {
+		f := flag.New()
+		v := viper.New()
+
+		v.Set(f.Vault.Address, tc.address)
+		v.Set(f.Vault.Token, tc.token)
+
+		config := Config{
+			Flag:  f,
+			Viper: v,
+		}
+
+		vaultClient, err := NewClient(config)
+		if tc.expectedError {
+			assert.Error(t, err, fmt.Sprintf("[%s] An error was expected", tc.name))
+			continue
+		} else {
+			assert.NotNil(t, vaultClient, "Vault client is nil but should not be")
+			assert.Nil(t, err, "Unexpected error creating vault client")
+		}
+	}
+}
