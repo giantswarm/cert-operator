5527fb54c357043b40f912d4855eb1e60db55ee2
rossf7 <ross@giantswarm.io>
2017-03-22 16:47:39 +0100

    Improve test for getting allowed domains for the CA. Remove test for getting the CA common name as func has been simplified.

--- service/create/pki_test.go
+++ service/create/pki_test.go
@@ -8,76 +8,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetClusterCA(t *testing.T) {
-	svc, _ := New(DefaultConfig())
-
-	tests := []struct {
-		name           string
-		certCommonName string
-		expectedCA     string
-	}{
-		{
-			name:           "Specify valid api cert",
-			certCommonName: "api.test-cluster.g8s.eu-west-1.aws.test.private.giantswarm.io",
-			expectedCA:     "test-cluster.g8s.eu-west-1.aws.test.private.giantswarm.io",
-		},
-		{
-			name:           "Specify valid etcd cert",
-			certCommonName: "etcd.test-cluster.g8s.eu-west-1.aws.test.private.giantswarm.io",
-			expectedCA:     "test-cluster.g8s.eu-west-1.aws.test.private.giantswarm.io",
-		},
-		{
-			name:           "Specify different url structure",
-			certCommonName: "api.test-cluster.private.giantswarm.io",
-			expectedCA:     "test-cluster.private.giantswarm.io",
-		},
-	}
-
-	for _, tc := range tests {
-		cert := CertificateSpec{
-			CommonName: tc.certCommonName,
-		}
-
-		ca := svc.getClusterCA(cert)
-		assert.Equal(t, tc.expectedCA, ca, fmt.Sprintf("[%s] CA common name should be equal", tc.name))
-	}
-}
-
 func TestGetAllowedDomainsForCA(t *testing.T) {
-	svc, _ := New(DefaultConfig())
-
 	tests := []struct {
 		name                string
-		certCommonName      string
-		altNames            []string
+		caCommonName        string
+		cert                CertificateSpec
 		expectedDomainCount int
 	}{
 		{
-			name:           "Specify valid api cert",
-			certCommonName: "api.test-cluster.g8s.eu-west-1.aws.test.private.giantswarm.io",
-			altNames: []string{
-				"kubernetes",
-				"kubernetes.default",
-				"kubernetes.default.svc",
-				"kubernetes.default.svc.cluster.local",
+			name:         "Specify api cert with alt names",
+			caCommonName: "cluster-test.g8s.test.giantswarm.io",
+			cert: CertificateSpec{
+				AltNames: []string{
+					"kubernetes",
+					"kubernetes.default",
+					"kubernetes.default.svc",
+					"kubernetes.default.svc.cluster.local",
+				},
 			},
 			expectedDomainCount: 5,
 		},
 		{
-			name:                "Specify etcd cert without allowed names",
-			certCommonName:      "etcd.test-cluster.g8s.eu-west-1.aws.test.private.giantswarm.io",
-			altNames:            []string{},
+			name:                "Specify etcd cert without alt names",
+			caCommonName:        "cluster-test.g8s.test.giantswarm.io",
+			cert:                CertificateSpec{},
 			expectedDomainCount: 1,
 		},
 	}
 
 	for _, tc := range tests {
-		cert := CertificateSpec{
-			CommonName: tc.certCommonName,
-			AltNames:   tc.altNames,
-		}
-
-		allowedDomains := svc.getAllowedDomainsForCA(cert)
+		allowedDomains := getAllowedDomainsForCA(tc.caCommonName, tc.cert)
 		domainCount := len(strings.Split(allowedDomains, ","))
 
 		assert.Equal(t, tc.expectedDomainCount, domainCount, fmt.Sprintf("[%s] Allowed domains should be equal", tc.name))
