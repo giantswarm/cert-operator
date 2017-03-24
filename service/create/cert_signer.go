aefe74bad97c90b01b163570a17ab8a5140fb9f1
Ross Fairbanks <rossf7@users.noreply.github.com>
2017-03-24 16:28:08 +0100

    Save certificates as k8s secrets and implement delete watch (#15)
    * addFunc saves the certificate as a k8s secret
    
    * deleteFunc deletes the k8s secret that stores the certificate
    
    * Creating and deleting secrets should both be idempotent
    
    * Label the secret with clusterComponent received from the TPO
    
    * Updated certificatetpr dev

--- service/create/cert_signer.go
+++ service/create/cert_signer.go
@@ -10,32 +10,40 @@ import (
 )
 
 // Issue generates a certificate using the PKI backend signed by the certificate
-// authority associated with the configured cluster ID.
-func (s *Service) Issue(cert certificatetpr.Spec) (spec.IssueResponse, error) {
-	var issueResp spec.IssueResponse
-
-	// Create a certificate signer to generate a new signed certificate.
+// authority associated with the configured cluster ID. The certificate is saved
+// as a set of k8s secrets.
+func (s *Service) Issue(cert *certificatetpr.CustomObject) error {
 	newCertSignerConfig := certsigner.DefaultConfig()
 	newCertSignerConfig.VaultClient = s.Config.VaultClient
 
 	newCertSigner, err := certsigner.New(newCertSignerConfig)
 	if err != nil {
-		return issueResp, microerror.MaskAny(err)
+		return microerror.MaskAny(err)
 	}
 
 	// Generate a new signed certificate.
 	newIssueConfig := spec.IssueConfig{
-		ClusterID:  cert.ClusterID,
-		CommonName: cert.CommonName,
-		IPSANs:     strings.Join(cert.IPSANs, ","),
-		AltNames:   strings.Join(cert.AltNames, ","),
-		TTL:        cert.TTL,
+		ClusterID:  cert.Spec.ClusterID,
+		CommonName: cert.Spec.CommonName,
+		IPSANs:     strings.Join(cert.Spec.IPSANs, ","),
+		AltNames:   strings.Join(cert.Spec.AltNames, ","),
+		TTL:        cert.Spec.TTL,
 	}
-
 	newIssueResponse, err := newCertSigner.Issue(newIssueConfig)
 	if err != nil {
-		return issueResp, microerror.MaskAny(err)
+		return microerror.MaskAny(err)
 	}
 
-	return newIssueResponse, err
+	// Save the certificate as a k8s secret.
+	secret := certificateSecret{
+		ClusterComponent: cert.Spec.ClusterComponent,
+		CommonName:       cert.Spec.CommonName,
+		Namespace:        cert.ObjectMeta.Namespace,
+		IssueResponse:    newIssueResponse,
+	}
+	if err := s.CreateCertificate(secret); err != nil {
+		return microerror.MaskAny(err)
+	}
+
+	return nil
 }
---
+++ service/create/k8s_secret.go
@@ -0,0 +1,53 @@
+package create
+
+import (
+	"github.com/giantswarm/certificatetpr"
+	microerror "github.com/giantswarm/microkit/error"
+	"k8s.io/client-go/pkg/api/errors"
+	"k8s.io/client-go/pkg/api/v1"
+)
+
+// CreateCertificate saves the certificate as a k8s secret.
+func (s *Service) CreateCertificate(cert certificateSecret) error {
+	var err error
+
+	secret := &v1.Secret{
+		ObjectMeta: v1.ObjectMeta{
+			Name: cert.CommonName,
+			Labels: map[string]string{
+				"clusterComponent": cert.ClusterComponent,
+			},
+		},
+		StringData: map[string]string{
+			"crt": cert.IssueResponse.Certificate,
+			"key": cert.IssueResponse.PrivateKey,
+			"ca":  cert.IssueResponse.IssuingCA,
+		},
+	}
+
+	// Create the secret which should be idempotent.
+	_, err = s.Config.K8sClient.Core().Secrets(cert.Namespace).Create(secret)
+	if errors.IsAlreadyExists(err) {
+		return nil
+	} else if err != nil {
+		return microerror.MaskAny(err)
+	}
+
+	return nil
+}
+
+// DeleteCertificate deletes the k8s secret that stores the certificate.
+func (s *Service) DeleteCertificate(cert *certificatetpr.CustomObject) error {
+	namespace := cert.ObjectMeta.Namespace
+	secretName := cert.Spec.CommonName
+
+	// Delete the secret which should be idempotent.
+	err := s.Config.K8sClient.Core().Secrets(namespace).Delete(secretName, &v1.DeleteOptions{})
+	if errors.IsNotFound(err) {
+		return nil
+	} else if err != nil {
+		return microerror.MaskAny(err)
+	}
+
+	return nil
+}
