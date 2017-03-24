95efa19a027c6c49518a92a9038f77de4d516396
rossf7 <ross@giantswarm.io>
2017-03-24 16:15:13 +0100

    Updated certificatetpr dev

--- vendor/github.com/giantswarm/certificatetpr/spec.go
+++ vendor/github.com/giantswarm/certificatetpr/spec.go
@@ -3,6 +3,7 @@ package certificatetpr
 type Spec struct {
 	AllowBareDomains bool     `json:"allowBareDomains" yaml:"allowBareDomains"`
 	AltNames         []string `json:"altNames" yaml:"altNames"`
+	ClusterComponent string   `json:"clusterComponent" yaml:"clusterComponent"`
 	ClusterID        string   `json:"clusterID" yaml:"clusterID"`
 	CommonName       string   `json:"commonName" yaml:"commonName"`
 	IPSANs           []string `json:"ipSans" yaml:"ipSans"`
