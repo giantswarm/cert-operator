a5dcea8ddc0c01f5fa680410871fedd25a38b20c
Tim Schindler <xh3b4sd@gmail.com>
2017-10-16 10:20:23 +0200

    added basic test structure for vaultcrt resource

--- service/resource/vaultcrt/delete_test.go
+++ service/resource/vaultcrt/delete_test.go
@@ -2,95 +2,33 @@ package vaultcrt
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
-	"github.com/giantswarm/flanneltpr"
-	flanneltprspec "github.com/giantswarm/flanneltpr/spec"
+	"github.com/giantswarm/certificatetpr"
 	"github.com/giantswarm/micrologger/microloggertest"
-	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/giantswarm/vaultcrt/vaultcrttest"
+	"github.com/giantswarm/vaultrole/vaultroletest"
 	"k8s.io/client-go/kubernetes/fake"
 	apiv1 "k8s.io/client-go/pkg/api/v1"
 )
 
 func Test_Resource_Namespace_GetDeleteState(t *testing.T) {
 	testCases := []struct {
-		Obj               interface{}
-		Cur               interface{}
-		Des               interface{}
-		ExpectedNamespace *apiv1.Namespace
+		Obj            interface{}
+		CurrentState   interface{}
+		DesiredState   interface{}
+		ExpectedSecret *apiv1.Secret
 	}{
 		{
-			Obj: &flanneltpr.CustomObject{
-				Spec: flanneltpr.Spec{
-					Cluster: flanneltprspec.Cluster{
-						ID: "foobar",
-					},
+			Obj: &certificatetpr.CustomObject{
+				Spec: certificatetpr.Spec{
+					ClusterID: "foobar",
 				},
 			},
-			Cur: &apiv1.Namespace{
-				TypeMeta: apismetav1.TypeMeta{
-					Kind:       "Namespace",
-					APIVersion: "v1",
-				},
-				ObjectMeta: apismetav1.ObjectMeta{
-					Name: "al9qy",
-					Labels: map[string]string{
-						"cluster":  "al9qy",
-						"customer": "test-customer",
-					},
-				},
-			},
-			Des: &apiv1.Namespace{
-				TypeMeta: apismetav1.TypeMeta{
-					Kind:       "Namespace",
-					APIVersion: "v1",
-				},
-				ObjectMeta: apismetav1.ObjectMeta{
-					Name: "al9qy",
-					Labels: map[string]string{
-						"cluster":  "al9qy",
-						"customer": "test-customer",
-					},
-				},
-			},
-			ExpectedNamespace: &apiv1.Namespace{
-				TypeMeta: apismetav1.TypeMeta{
-					Kind:       "Namespace",
-					APIVersion: "v1",
-				},
-				ObjectMeta: apismetav1.ObjectMeta{
-					Name: "al9qy",
-					Labels: map[string]string{
-						"cluster":  "al9qy",
-						"customer": "test-customer",
-					},
-				},
-			},
-		},
-
-		{
-			Obj: &flanneltpr.CustomObject{
-				Spec: flanneltpr.Spec{
-					Cluster: flanneltprspec.Cluster{
-						ID: "foobar",
-					},
-				},
-			},
-			Cur: nil,
-			Des: &apiv1.Namespace{
-				TypeMeta: apismetav1.TypeMeta{
-					Kind:       "Namespace",
-					APIVersion: "v1",
-				},
-				ObjectMeta: apismetav1.ObjectMeta{
-					Name: "al9qy",
-					Labels: map[string]string{
-						"cluster":  "al9qy",
-						"customer": "test-customer",
-					},
-				},
-			},
-			ExpectedNamespace: nil,
+			CurrentState:   &apiv1.Secret{},
+			DesiredState:   &apiv1.Secret{},
+			ExpectedSecret: &apiv1.Secret{},
 		},
 	}
 
@@ -98,8 +36,12 @@ func Test_Resource_Namespace_GetDeleteState(t *testing.T) {
 	var newResource *Resource
 	{
 		resourceConfig := DefaultConfig()
+
 		resourceConfig.K8sClient = fake.NewSimpleClientset()
 		resourceConfig.Logger = microloggertest.New()
+		resourceConfig.VaultCrt = vaultcrttest.New()
+		resourceConfig.VaultRole = vaultroletest.New()
+
 		newResource, err = New(resourceConfig)
 		if err != nil {
 			t.Fatal("expected", nil, "got", err)
@@ -107,19 +49,13 @@ func Test_Resource_Namespace_GetDeleteState(t *testing.T) {
 	}
 
 	for i, tc := range testCases {
-		result, err := newResource.GetDeleteState(context.TODO(), tc.Obj, tc.Cur, tc.Des)
+		result, err := newResource.GetDeleteState(context.TODO(), tc.Obj, tc.CurrentState, tc.DesiredState)
 		if err != nil {
 			t.Fatal("case", i+1, "expected", nil, "got", err)
 		}
-		if tc.ExpectedNamespace == nil {
-			if tc.ExpectedNamespace != result {
-				t.Fatal("case", i+1, "expected", tc.ExpectedNamespace, "got", result)
-			}
-		} else {
-			name := result.(*apiv1.Namespace).Name
-			if tc.ExpectedNamespace.Name != name {
-				t.Fatal("case", i+1, "expected", tc.ExpectedNamespace.Name, "got", name)
-			}
+		secret := result.(*apiv1.Secret)
+		if !reflect.DeepEqual(tc.ExpectedSecret, secret) {
+			t.Fatalf("case %d expected %#v got %#v", i+1, tc.ExpectedSecret, secret)
 		}
 	}
 }
