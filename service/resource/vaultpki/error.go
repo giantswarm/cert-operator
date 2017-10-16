e935453b2a4dee2f94821d4a79b0e5aeeb7163d7
Tim Schindler <xh3b4sd@gmail.com>
2017-10-16 11:20:14 +0200

    removed unused error matcher

--- service/resource/vaultpki/error.go
+++ service/resource/vaultpki/error.go
@@ -1,8 +1,6 @@
 package vaultpki
 
 import (
-	"strings"
-
 	"github.com/giantswarm/microerror"
 )
 
@@ -18,17 +16,4 @@ var wrongTypeError = microerror.New("wrong type")
 // IsWrongTypeError asserts wrongTypeError.
 func IsWrongTypeError(err error) bool {
 	return microerror.Cause(err) == wrongTypeError
-}
-
-// IsNoVaultHandlerDefined asserts a dirty string matching against the error
-// message provided by err. This is necessary due to the poor error handling
-// design of the Vault library we are using.
-func IsNoVaultHandlerDefined(err error) bool {
-	cause := microerror.Cause(err)
-
-	if cause != nil && strings.Contains(cause.Error(), "no handler for route") {
-		return true
-	}
-
-	return false
 }
