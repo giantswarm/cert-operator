f633bc1641d5d0952560bd96d16988cedff4d145
Julien Garcia Gonzalez <julien@giantswarm.io>
2017-11-17 15:16:42 +0100

    add test
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

---
+++ service/key/key_test.go
@@ -0,0 +1,55 @@
+package key
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/giantswarm/certificatetpr"
+)
+
+func Test_Organization_sort(t *testing.T) {
+	testCases := []struct {
+		Organizations         []string
+		ClusterComponent      string
+		ExpectedOrganizations []string
+	}{
+		{
+			Organizations:         []string{},
+			ClusterComponent:      "api",
+			ExpectedOrganizations: []string{"api"},
+		},
+		{
+			Organizations:         []string{"system:master"},
+			ClusterComponent:      "api",
+			ExpectedOrganizations: []string{"api", "system:master"},
+		},
+		{
+			Organizations:         []string{"system:master", "giantswarm"},
+			ClusterComponent:      "api",
+			ExpectedOrganizations: []string{"api", "system:master", "giantswarm"},
+		},
+	}
+
+	for i, tc := range testCases {
+		customObject := certificatetpr.CustomObject{
+			Spec: certificatetpr.Spec{
+				ClusterComponent: tc.ClusterComponent,
+				Organizations:    tc.Organizations,
+			},
+		}
+
+		validate := func(result []string) {
+			if !reflect.DeepEqual(tc.ExpectedOrganizations, result) {
+				t.Fatalf("case %d expected %#v got %#v", i, tc.ExpectedOrganizations, result)
+			}
+		}
+		result := Organizations(customObject)
+		validate(result)
+
+		result = Organizations(customObject)
+		validate(result)
+
+		result = Organizations(customObject)
+		validate(result)
+	}
+}
