51ad561ae1733de13df8b8e8d6d5fc24fd429c1a
Tim Schindler <xh3b4sd@gmail.com>
2017-12-01 10:27:13 +0100

    updated vendor deps

--- vendor/github.com/giantswarm/apiextensions/pkg/apis/cluster/v1alpha1/aws_types.go
+++ vendor/github.com/giantswarm/apiextensions/pkg/apis/cluster/v1alpha1/aws_types.go
@@ -7,39 +7,39 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// NewAWSCRD returns a new custom resource definition for AWS. This might look
-// something like the following.
+// NewAWSConfigCRD returns a new custom resource definition for AWSConfig. This
+// might look something like the following.
 //
 //     apiVersion: apiextensions.k8s.io/v1beta1
 //     kind: CustomResourceDefinition
 //     metadata:
-//       name: awss.cluster.giantswarm.io
+//       name: awsconfigs.cluster.giantswarm.io
 //     spec:
 //       group: cluster.giantswarm.io
 //       scope: Namespaced
 //       version: v1alpha1
 //       names:
-//         kind: AWS
-//         plural: awss
-//         singular: aws
+//         kind: AWSConfig
+//         plural: awsconfigs
+//         singular: awsconfig
 //
-func NewAWSCRD() *apiextensionsv1beta1.CustomResourceDefinition {
+func NewAWSConfigCRD() *apiextensionsv1beta1.CustomResourceDefinition {
 	return &apiextensionsv1beta1.CustomResourceDefinition{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: apiextensionsv1beta1.SchemeGroupVersion.String(),
 			Kind:       "CustomResourceDefinition",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "awss.cluster.giantswarm.io",
+			Name: "awsconfigs.cluster.giantswarm.io",
 		},
 		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
 			Group:   "cluster.giantswarm.io",
 			Scope:   "Namespaced",
 			Version: "v1alpha1",
 			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
-				Kind:     "AWS",
-				Plural:   "awss",
-				Singular: "aws",
+				Kind:     "AWSConfig",
+				Plural:   "awsconfigs",
+				Singular: "awsconfig",
 			},
 		},
 	}
@@ -49,69 +49,69 @@ func NewAWSCRD() *apiextensionsv1beta1.CustomResourceDefinition {
 // +genclient:noStatus
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-type AWS struct {
+type AWSConfig struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
-	Spec              AWSSpec `json:"spec"`
+	Spec              AWSConfigSpec `json:"spec"`
 }
 
-type AWSSpec struct {
-	Cluster       AWSSpecCluster       `json:"cluster" yaml:"cluster"`
-	AWS           AWSSpecAWS           `json:"aws" yaml:"aws"`
-	VersionBundle AWSSpecVersionBundle `json:"versionBundle" yaml:"versionBundle"`
+type AWSConfigSpec struct {
+	Cluster       AWSConfigSpecCluster       `json:"cluster" yaml:"cluster"`
+	AWS           AWSConfigSpecAWS           `json:"aws" yaml:"aws"`
+	VersionBundle AWSConfigSpecVersionBundle `json:"versionBundle" yaml:"versionBundle"`
 }
 
-type AWSSpecCluster struct {
-	Calico     AWSSpecClusterCalico     `json:"calico" yaml:"calico"`
-	Customer   AWSSpecClusterCustomer   `json:"customer" yaml:"customer"`
-	Docker     AWSSpecClusterDocker     `json:"docker" yaml:"docker"`
-	Etcd       AWSSpecClusterEtcd       `json:"etcd" yaml:"etcd"`
-	ID         string                   `json:"id" yaml:"id"`
-	Kubernetes AWSSpecClusterKubernetes `json:"kubernetes" yaml:"kubernetes"`
-	Masters    []AWSSpecClusterNode     `json:"masters" yaml:"masters"`
-	Vault      AWSSpecClusterVault      `json:"vault" yaml:"vault"`
-	Workers    []AWSSpecClusterNode     `json:"workers" yaml:"workers"`
+type AWSConfigSpecCluster struct {
+	Calico     AWSConfigSpecClusterCalico     `json:"calico" yaml:"calico"`
+	Customer   AWSConfigSpecClusterCustomer   `json:"customer" yaml:"customer"`
+	Docker     AWSConfigSpecClusterDocker     `json:"docker" yaml:"docker"`
+	Etcd       AWSConfigSpecClusterEtcd       `json:"etcd" yaml:"etcd"`
+	ID         string                         `json:"id" yaml:"id"`
+	Kubernetes AWSConfigSpecClusterKubernetes `json:"kubernetes" yaml:"kubernetes"`
+	Masters    []AWSConfigSpecClusterNode     `json:"masters" yaml:"masters"`
+	Vault      AWSConfigSpecClusterVault      `json:"vault" yaml:"vault"`
+	Workers    []AWSConfigSpecClusterNode     `json:"workers" yaml:"workers"`
 }
 
-type AWSSpecClusterCalico struct {
+type AWSConfigSpecClusterCalico struct {
 	CIDR   int    `json:"cidr" yaml:"cidr"`
 	Domain string `json:"domain" yaml:"domain"`
 	MTU    int    `json:"mtu" yaml:"mtu"`
 	Subnet string `json:"subnet" yaml:"subnet"`
 }
 
-type AWSSpecClusterCustomer struct {
+type AWSConfigSpecClusterCustomer struct {
 	ID string `json:"id" yaml:"id"`
 }
 
-type AWSSpecClusterDocker struct {
-	Daemon AWSSpecClusterDockerDaemon `json:"daemon" yaml:"daemon"`
+type AWSConfigSpecClusterDocker struct {
+	Daemon AWSConfigSpecClusterDockerDaemon `json:"daemon" yaml:"daemon"`
 }
 
-type AWSSpecClusterDockerDaemon struct {
+type AWSConfigSpecClusterDockerDaemon struct {
 	CIDR      string `json:"cidr" yaml:"cidr"`
 	ExtraArgs string `json:"extraArgs" yaml:"extraArgs"`
 }
 
-type AWSSpecClusterEtcd struct {
+type AWSConfigSpecClusterEtcd struct {
 	AltNames string `json:"altNames" yaml:"altNames"`
 	Domain   string `json:"domain" yaml:"domain"`
 	Port     int    `json:"port" yaml:"port"`
 	Prefix   string `json:"prefix" yaml:"prefix"`
 }
 
-type AWSSpecClusterKubernetes struct {
-	API               AWSSpecClusterKubernetesAPI               `json:"api" yaml:"api"`
-	DNS               AWSSpecClusterKubernetesDNS               `json:"dns" yaml:"dns"`
-	Domain            string                                    `json:"domain" yaml:"domain"`
-	Hyperkube         AWSSpecClusterKubernetesHyperkube         `json:"hyperkube" yaml:"hyperkube"`
-	IngressController AWSSpecClusterKubernetesIngressController `json:"ingressController" yaml:"ingressController"`
-	Kubelet           AWSSpecClusterKubernetesKubelet           `json:"kubelet" yaml:"kubelet"`
-	NetworkSetup      AWSSpecClusterKubernetesNetworkSetup      `json:"networkSetup" yaml:"networkSetup"`
-	SSH               AWSSpecClusterKubernetesSSH               `json:"ssh" yaml:"ssh"`
+type AWSConfigSpecClusterKubernetes struct {
+	API               AWSConfigSpecClusterKubernetesAPI               `json:"api" yaml:"api"`
+	DNS               AWSConfigSpecClusterKubernetesDNS               `json:"dns" yaml:"dns"`
+	Domain            string                                          `json:"domain" yaml:"domain"`
+	Hyperkube         AWSConfigSpecClusterKubernetesHyperkube         `json:"hyperkube" yaml:"hyperkube"`
+	IngressController AWSConfigSpecClusterKubernetesIngressController `json:"ingressController" yaml:"ingressController"`
+	Kubelet           AWSConfigSpecClusterKubernetesKubelet           `json:"kubelet" yaml:"kubelet"`
+	NetworkSetup      AWSConfigSpecClusterKubernetesNetworkSetup      `json:"networkSetup" yaml:"networkSetup"`
+	SSH               AWSConfigSpecClusterKubernetesSSH               `json:"ssh" yaml:"ssh"`
 }
 
-type AWSSpecClusterKubernetesAPI struct {
+type AWSConfigSpecClusterKubernetesAPI struct {
 	AltNames       string `json:"altNames" yaml:"altNames"`
 	ClusterIPRange string `json:"clusterIPRange" yaml:"clusterIPRange"`
 	Domain         string `json:"domain" yaml:"domain"`
@@ -120,107 +120,107 @@ type AWSSpecClusterKubernetesAPI struct {
 	SecurePort     int    `json:"securePort" yaml:"securePort"`
 }
 
-type AWSSpecClusterKubernetesDNS struct {
+type AWSConfigSpecClusterKubernetesDNS struct {
 	IP net.IP `json:"ip" yaml:"ip"`
 }
 
-type AWSSpecClusterKubernetesHyperkube struct {
-	Docker AWSSpecClusterKubernetesHyperkubeDocker `json:"docker" yaml:"docker"`
+type AWSConfigSpecClusterKubernetesHyperkube struct {
+	Docker AWSConfigSpecClusterKubernetesHyperkubeDocker `json:"docker" yaml:"docker"`
 }
 
-type AWSSpecClusterKubernetesHyperkubeDocker struct {
+type AWSConfigSpecClusterKubernetesHyperkubeDocker struct {
 	Image string `json:"image" yaml:"image"`
 }
 
-type AWSSpecClusterKubernetesIngressController struct {
-	Docker         AWSSpecClusterKubernetesIngressControllerDocker `json:"docker" yaml:"docker"`
-	Domain         string                                          `json:"domain" yaml:"domain"`
-	WildcardDomain string                                          `json:"wildcardDomain" yaml:"wildcardDomain"`
-	InsecurePort   int                                             `json:"insecurePort" yaml:"insecurePort"`
-	SecurePort     int                                             `json:"securePort" yaml:"securePort"`
+type AWSConfigSpecClusterKubernetesIngressController struct {
+	Docker         AWSConfigSpecClusterKubernetesIngressControllerDocker `json:"docker" yaml:"docker"`
+	Domain         string                                                `json:"domain" yaml:"domain"`
+	WildcardDomain string                                                `json:"wildcardDomain" yaml:"wildcardDomain"`
+	InsecurePort   int                                                   `json:"insecurePort" yaml:"insecurePort"`
+	SecurePort     int                                                   `json:"securePort" yaml:"securePort"`
 }
 
-type AWSSpecClusterKubernetesIngressControllerDocker struct {
+type AWSConfigSpecClusterKubernetesIngressControllerDocker struct {
 	Image string `json:"image" yaml:"image"`
 }
 
-type AWSSpecClusterKubernetesKubelet struct {
+type AWSConfigSpecClusterKubernetesKubelet struct {
 	AltNames string `json:"altNames" yaml:"altNames"`
 	Domain   string `json:"domain" yaml:"domain"`
 	Labels   string `json:"labels" yaml:"labels"`
 	Port     int    `json:"port" yaml:"port"`
 }
 
-type AWSSpecClusterKubernetesNetworkSetup struct {
-	Docker AWSSpecClusterKubernetesNetworkSetupDocker `json:"docker" yaml:"docker"`
+type AWSConfigSpecClusterKubernetesNetworkSetup struct {
+	Docker AWSConfigSpecClusterKubernetesNetworkSetupDocker `json:"docker" yaml:"docker"`
 }
 
-type AWSSpecClusterKubernetesNetworkSetupDocker struct {
+type AWSConfigSpecClusterKubernetesNetworkSetupDocker struct {
 	Image string `json:"image" yaml:"image"`
 }
 
-type AWSSpecClusterKubernetesSSH struct {
-	UserList []AWSSpecClusterKubernetesSSHUser `json:"userList" yaml:"userList"`
+type AWSConfigSpecClusterKubernetesSSH struct {
+	UserList []AWSConfigSpecClusterKubernetesSSHUser `json:"userList" yaml:"userList"`
 }
 
-type AWSSpecClusterKubernetesSSHUser struct {
+type AWSConfigSpecClusterKubernetesSSHUser struct {
 	Name      string `json:"name" yaml:"name"`
 	PublicKey string `json:"publicKey" yaml:"publicKey"`
 }
 
-type AWSSpecClusterNode struct {
+type AWSConfigSpecClusterNode struct {
 	ID string `json:"id" yaml:"id"`
 }
 
-type AWSSpecClusterVault struct {
+type AWSConfigSpecClusterVault struct {
 	Address string `json:"address" yaml:"address"`
 	Token   string `json:"token" yaml:"token"`
 }
 
-type AWSSpecAWS struct {
-	API     AWSSpecAWSAPI     `json:"api" yaml:"api"`
-	AZ      string            `json:"az" yaml:"az"`
-	Etcd    AWSSpecAWSEtcd    `json:"etcd" yaml:"etcd"`
-	Ingress AWSSpecAWSIngress `json:"ingress" yaml:"ingress"`
-	Masters []AWSSpecAWSNode  `json:"masters" yaml:"masters"`
-	Region  string            `json:"region" yaml:"region"`
-	VPC     AWSSpecAWSVPC     `json:"vpc" yaml:"vpc"`
-	Workers []AWSSpecAWSNode  `json:"workers" yaml:"workers"`
+type AWSConfigSpecAWS struct {
+	API     AWSConfigSpecAWSAPI     `json:"api" yaml:"api"`
+	AZ      string                  `json:"az" yaml:"az"`
+	Etcd    AWSConfigSpecAWSEtcd    `json:"etcd" yaml:"etcd"`
+	Ingress AWSConfigSpecAWSIngress `json:"ingress" yaml:"ingress"`
+	Masters []AWSConfigSpecAWSNode  `json:"masters" yaml:"masters"`
+	Region  string                  `json:"region" yaml:"region"`
+	VPC     AWSConfigSpecAWSVPC     `json:"vpc" yaml:"vpc"`
+	Workers []AWSConfigSpecAWSNode  `json:"workers" yaml:"workers"`
 }
 
-type AWSSpecAWSAPI struct {
-	HostedZones string           `json:"hostedZones" yaml:"hostedZones"`
-	ELB         AWSSpecAWSAPIELB `json:"elb" yaml:"elb"`
+type AWSConfigSpecAWSAPI struct {
+	HostedZones string                 `json:"hostedZones" yaml:"hostedZones"`
+	ELB         AWSConfigSpecAWSAPIELB `json:"elb" yaml:"elb"`
 }
 
-type AWSSpecAWSAPIELB struct {
+type AWSConfigSpecAWSAPIELB struct {
 	IdleTimeoutSeconds int `json:"idleTimeoutSeconds" yaml:"idleTimeoutSeconds"`
 }
 
-type AWSSpecAWSEtcd struct {
-	HostedZones string            `json:"hostedZones" yaml:"hostedZones"`
-	ELB         AWSSpecAWSEtcdELB `json:"elb" yaml:"elb"`
+type AWSConfigSpecAWSEtcd struct {
+	HostedZones string                  `json:"hostedZones" yaml:"hostedZones"`
+	ELB         AWSConfigSpecAWSEtcdELB `json:"elb" yaml:"elb"`
 }
 
-type AWSSpecAWSEtcdELB struct {
+type AWSConfigSpecAWSEtcdELB struct {
 	IdleTimeoutSeconds int `json:"idleTimeoutSeconds" yaml:"idleTimeoutSeconds"`
 }
 
-type AWSSpecAWSIngress struct {
-	HostedZones string               `json:"hostedZones" yaml:"hostedZones"`
-	ELB         AWSSpecAWSIngressELB `json:"elb" yaml:"elb"`
+type AWSConfigSpecAWSIngress struct {
+	HostedZones string                     `json:"hostedZones" yaml:"hostedZones"`
+	ELB         AWSConfigSpecAWSIngressELB `json:"elb" yaml:"elb"`
 }
 
-type AWSSpecAWSIngressELB struct {
+type AWSConfigSpecAWSIngressELB struct {
 	IdleTimeoutSeconds int `json:"idleTimeoutSeconds" yaml:"idleTimeoutSeconds"`
 }
 
-type AWSSpecAWSNode struct {
+type AWSConfigSpecAWSNode struct {
 	ImageID      string `json:"imageID" yaml:"imageID"`
 	InstanceType string `json:"instanceType" yaml:"instanceType"`
 }
 
-type AWSSpecAWSVPC struct {
+type AWSConfigSpecAWSVPC struct {
 	CIDR              string   `json:"cidr" yaml:"cidr"`
 	PrivateSubnetCIDR string   `json:"privateSubnetCidr" yaml:"privateSubnetCidr"`
 	PublicSubnetCIDR  string   `json:"publicSubnetCidr" yaml:"publicSubnetCidr"`
@@ -228,14 +228,14 @@ type AWSSpecAWSVPC struct {
 	PeerID            string   `json:"peerId" yaml:"peerId"`
 }
 
-type AWSSpecVersionBundle struct {
+type AWSConfigSpecVersionBundle struct {
 	Version string `json:"version" yaml:"version"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-type AWSList struct {
+type AWSConfigList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
-	Items           []AWS `json:"items"`
+	Items           []AWSConfig `json:"items"`
 }
