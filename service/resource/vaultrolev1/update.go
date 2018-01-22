e8ffdb6974c4586de6c46effea2ef010b71be8e2
Tim Schindler <xh3b4sd@gmail.com>
2018-01-22 18:43:12 +0100

    added basic implementation for ApplyUpdateChange (#149)

--- service/resource/vaultrolev1/update.go
+++ service/resource/vaultrolev1/update.go
@@ -2,12 +2,40 @@ package vaultrolev1
 
 import (
 	"context"
+	"reflect"
 
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/framework"
+	"github.com/giantswarm/vaultrole"
 )
 
 func (r *Resource) ApplyUpdateChange(ctx context.Context, obj, updateChange interface{}) error {
+	roleToUpdate, err := toRole(updateChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if roleToUpdate != nil {
+		r.logger.LogCtx(ctx, "debug", "updating the role in the Vault API")
+
+		c := vaultrole.UpdateConfig{
+			AllowBareDomains: roleToUpdate.AllowBareDomains,
+			AllowSubdomains:  roleToUpdate.AllowSubdomains,
+			AltNames:         roleToUpdate.AltNames,
+			ID:               roleToUpdate.ID,
+			Organizations:    roleToUpdate.Organizations,
+			TTL:              roleToUpdate.TTL.String(),
+		}
+		err = r.vaultRole.Update(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "debug", "updated the role in the Vault API")
+	} else {
+		r.logger.LogCtx(ctx, "debug", "the role does not need to be updated in the Vault API")
+	}
+
 	return nil
 }
 
@@ -30,5 +58,23 @@ func (r *Resource) NewUpdatePatch(ctx context.Context, obj, currentState, desire
 }
 
 func (r *Resource) newUpdateChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
-	return nil, nil
+	currentRole, err := toRole(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredRole, err := toRole(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "debug", "finding out if the role has to be updated")
+
+	var roleToUpdate *vaultrole.Role
+	if !reflect.DeepEqual(currentRole, desiredRole) {
+		roleToUpdate = desiredRole
+	}
+
+	r.logger.LogCtx(ctx, "debug", "found out if the role has to be updated")
+
+	return roleToUpdate, nil
 }
