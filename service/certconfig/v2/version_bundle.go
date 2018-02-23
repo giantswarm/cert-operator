6105d163fdabeb6b4d6fefa0aea3f620409b19b1
Tim Schindler <xh3b4sd@gmail.com>
2018-02-23 15:09:58 +0100

    moved version bundle definitions to versioned folders

---
+++ service/certconfig/v2/version_bundle.go
@@ -0,0 +1,31 @@
+package v2
+
+import (
+	"time"
+
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "vault",
+				Description: "Vault version updated.",
+				Kind:        "changed",
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "vault",
+				Version: "0.7.3",
+			},
+		},
+		Dependencies: []versionbundle.Dependency{},
+		Deprecated:   false,
+		Name:         "cert-operator",
+		Time:         time.Date(2017, time.October, 26, 16, 53, 0, 0, time.UTC),
+		Version:      "0.1.0",
+		WIP:          false,
+	}
+}
--- service/version_bundles.go
+++ service/version_bundles.go
@@ -1,33 +1,15 @@
 package service
 
 import (
-	"time"
-
 	"github.com/giantswarm/versionbundle"
+
+	"github.com/giantswarm/cert-operator/service/certconfig/v2"
 )
 
 func NewVersionBundles() []versionbundle.Bundle {
-	return []versionbundle.Bundle{
-		{
-			Changelogs: []versionbundle.Changelog{
-				{
-					Component:   "vault",
-					Description: "Vault version updated.",
-					Kind:        "changed",
-				},
-			},
-			Components: []versionbundle.Component{
-				{
-					Name:    "vault",
-					Version: "0.7.3",
-				},
-			},
-			Dependencies: []versionbundle.Dependency{},
-			Deprecated:   false,
-			Name:         "cert-operator",
-			Time:         time.Date(2017, time.October, 26, 16, 53, 0, 0, time.UTC),
-			Version:      "0.1.0",
-			WIP:          false,
-		},
-	}
+	var versionBundles []versionbundle.Bundle
+
+	versionBundles = append(versionBundles, v2.VersionBundle())
+
+	return versionBundles
 }
