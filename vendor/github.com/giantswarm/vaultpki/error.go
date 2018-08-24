50f9e59d147788f5326f9516699ebf789991d874
Tim Schindler <xh3b4sd@gmail.com>
2018-08-24 09:59:27 +0200

    updates vendor deps

--- vendor/github.com/giantswarm/vaultpki/error.go
+++ vendor/github.com/giantswarm/vaultpki/error.go
@@ -6,21 +6,27 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-var executionFailedError = microerror.New("execution failed")
+var executionFailedError = &microerror.Error{
+	Kind: "executionFailedError",
+}
 
 // IsExecutionFailed asserts executionFailedError.
 func IsExecutionFailed(err error) bool {
 	return microerror.Cause(err) == executionFailedError
 }
 
-var invalidConfigError = microerror.New("invalid config")
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
+}
 
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
 
-var notFoundError = microerror.New("not found")
+var notFoundError = &microerror.Error{
+	Kind: "notFoundError",
+}
 
 // IsNotFound asserts notFoundError.
 func IsNotFound(err error) bool {
