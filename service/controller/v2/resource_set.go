155f582ba0b3657ba6d132f1cee59440b53cf4d6
Tim Schindler <xh3b4sd@gmail.com>
2019-01-17 21:37:23 +0100

    compiles

--- service/controller/v2/resource_set.go
+++ service/controller/v2/resource_set.go
@@ -137,9 +137,7 @@ func NewResourceSet(config ResourceSetConfig) (*controller.ResourceSet, error) {
 	}
 
 	{
-		c := metricsresource.WrapConfig{
-			Name: config.ProjectName,
-		}
+		c := metricsresource.WrapConfig{}
 
 		resources, err = metricsresource.Wrap(resources, c)
 		if err != nil {
