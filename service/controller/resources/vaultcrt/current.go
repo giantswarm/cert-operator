39be970556423fdc91a0270e43ded88b68c04ed9
Theo Brigitte <theo.brigitte@gmail.com>
2020-04-23 15:51:19 +0200

    rename v2 imports

--- service/controller/resources/vaultcrt/current.go
+++ service/controller/resources/vaultcrt/current.go
@@ -15,7 +15,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/giantswarm/cert-operator/service/controller/v2/key"
+	"github.com/giantswarm/cert-operator/service/controller/key"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
