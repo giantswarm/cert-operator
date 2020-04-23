bfc10fc2ae40cb4751e35aa8cd561598d24ec8a1
wtty-fool <wtty.fool@gmail.com>
2020-04-23 12:58:07 +0200

    change version to v1.1.0-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "cert-operator"
 	source      string = "https://github.com/giantswarm/cert-operator"
-	version            = "n/a"
+	version            = "1.1.0-dev"
 )
 
 func Description() string {
