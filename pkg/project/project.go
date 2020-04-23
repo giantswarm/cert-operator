e96cdcf6b719defc57a0cf6bc9b97872c8d2b531
Jakub Soboń <wtty.fool@gmail.com>
2020-04-23 17:09:26 +0200

    Update pkg/project/project.go
    Co-Authored-By: Paweł Kopiczko <pawel@giantswarm.io>

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "cert-operator"
 	source      string = "https://github.com/giantswarm/cert-operator"
-	version            = "1.1.0-dev"
+	version            = "1.0.1-dev"
 )
 
 func Description() string {
