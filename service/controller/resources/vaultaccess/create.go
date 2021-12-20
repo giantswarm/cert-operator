ec40d9963b6c6364dbf1cff96da7776972ba33ea
Christian Bianchi <whites11@gmail.com>
2021-12-20 09:44:30 +0100

    Use `RenewSelf` instead of `LookupSelf` to prevent expiration of `Vault token`.

--- service/controller/resources/vaultaccess/create.go
+++ service/controller/resources/vaultaccess/create.go
@@ -8,6 +8,7 @@ import (
 )
 
 func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	r.logger.LogCtx(ctx, "level", "debug", "message", "renewing the Vault token")
 	_, err := r.vaultClient.Auth().Token().RenewSelf(0)
 	if IsVaultAccess(err) {
 		r.logger.LogCtx(ctx, "level", "debug", "message", "vault not reachable")
@@ -19,6 +20,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	} else if err != nil {
 		return microerror.Mask(err)
 	}
+	r.logger.LogCtx(ctx, "level", "debug", "message", "renewed the Vault token")
 
 	return nil
 }
