e1c008220c0a590cd5dda650eff0708adb785c0f
GitHub Action <action@github.com>
2022-01-03 15:14:38 +0000

    Bump version to 1.3.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "cert-operator"
 	source      string = "https://github.com/giantswarm/cert-operator"
-	version            = "1.3.0"
+	version            = "1.3.1-dev"
 )
 
 func Description() string {
