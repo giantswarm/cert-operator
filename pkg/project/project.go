c821f6aa85d27e75bef8c06fe29ad249c1b0fdfc
Christian Bianchi <whites11@gmail.com>
2022-11-22 15:33:06 +0100

    bump deps for nancy

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "cert-operator"
 	source      string = "https://github.com/giantswarm/cert-operator"
-	version            = "2.0.1"
+	version            = "2.0.2-dev"
 )
 
 func Description() string {
