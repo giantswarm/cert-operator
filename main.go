935cc25372b0591d227527bfee7819783170760f
Christian Bianchi <whites11@gmail.com>
2022-11-23 14:06:44 +0100

    Add possibility to run cert-operator as a unique app, reconciling special version '0.0.0'.

--- main.go
+++ main.go
@@ -109,6 +109,7 @@ func main() {
 	daemonCommand.PersistentFlags().Duration(f.Service.Resource.VaultCrt.ExpirationThreshold, 0, "Amount of time to renew certificates before their expiration date.")
 	daemonCommand.PersistentFlags().String(f.Service.Resource.VaultCrt.Namespace, "", "Namespace used to manage Kubernetes secrets in.")
 
+	daemonCommand.PersistentFlags().Bool(f.Service.App.Unique, false, "Whether the operator is deployed as a unique app.")
 	daemonCommand.PersistentFlags().String(f.Service.Vault.Config.Address, "", "Address used to connect to Vault.")
 	daemonCommand.PersistentFlags().String(f.Service.Vault.Config.Token, "", "Token used to authenticate against Vault.")
 	daemonCommand.PersistentFlags().String(f.Service.Vault.Config.PKI.CA.TTL, "", "TTL used to generate a new Cluster CA.")
