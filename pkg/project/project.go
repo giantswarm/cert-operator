50ce6ed9780fcada7fee8edfa18d834a2388ab0c
taylorbot <dev@giantswarm.io>
2024-03-28 14:35:19 +0000

    Release v3.4.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "cert-operator"
 	source      string = "https://github.com/giantswarm/cert-operator"
-	version            = "3.3.1-dev"
+	version            = "3.4.0"
 )
 
 func Description() string {
